b5d0faa0cd3ebd215bc4196af331318b4106f05e
Krel24 <kenyroselpc100@gmail.com>
2026-09-16 11:05:34 -0500

    edicion problema

--- PreparaciónLab02/FibonacciDP/fibonacci.cpp
+++ PreparaciónLab02/FibonacciDP/fibonacci.cpp
@@ -9,10 +9,13 @@ using namespace std;
 
 int main() {
     int N=8;
-    int F[N]={0,1};
-    for(int i=2;i<N;i++) {
+    int F[N+1]={};
+    F[0]=0;
+    F[1]=1;
+    for(int i=2;i<N+1;i++) {
         F[i]=F[i-1]+F[i-2];
+        cout<<"F["<<i<<"]="<<F[i]<<endl;
     }
-    cout<<"El número "<<N<<" de la funcion Fibo es: "<<F[N]<<endl;
+    cout<<"El numero "<<N<<" de la funcion Fibo es: "<<F[N]<<endl;
 
 }
