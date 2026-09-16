8bd989405c13c30cae271e75dfa4d6e64a8e4a25
Krel24 <kenyroselpc100@gmail.com>
2026-09-16 11:02:46 -0500

    edicion problema

--- PreparaciónLab02/FibonacciDP/fibonacci.cpp
+++ PreparaciónLab02/FibonacciDP/fibonacci.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "fibonacci.h"
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N=8;
+    int F[N]={0,1};
+    for(int i=2;i<N;i++) {
+        F[i]=F[i-1]+F[i-2];
+    }
+    cout<<"El número "<<N<<" de la funcion Fibo es: "<<F[N]<<endl;
+
+}
