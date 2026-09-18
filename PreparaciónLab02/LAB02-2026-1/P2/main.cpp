2a1d3597da2305bad44f301cb7c8c62b7d987297
Krel24 <kenyroselpc100@gmail.com>
2026-09-18 18:11:38 -0500

    edicion problema

--- PreparaciónLab02/LAB02-2026-1/P2/main.cpp
+++ PreparaciónLab02/LAB02-2026-1/P2/main.cpp
@@ -14,7 +14,7 @@ int main() {
     //llenamos el dp
     for (int i=0;i<n+1; i++) dp[i][0]=1;
     for (int j=1;j<totalA+1; j++) dp[0][j]=0;
-
+     //falta completar, de paso tambien el de dolares e intentar hacer unos tú solo
     for (int i=1; i<n+1; i++) {
         for (int j=1; j<totalA+1; j++) {
             if (j<numActas[i]) {
