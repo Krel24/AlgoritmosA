b2984f9a0cee0a3af4556287ce10a9241e06d6a0
Krel24 <kenyroselpc100@gmail.com>
2026-09-18 13:43:27 -0500

    edicion problema

---
+++ PreparaciónLab02/LAB02-2026-1/P2/main.cpp
@@ -0,0 +1,28 @@
+//
+// Created by User on 18/09/2026.
+//
+
+#include "main.h"
+#include <iomanip>
+using namespace std;
+int main() {
+    int totalA=95000, lima=35200, extran=2300;
+    int n=8;
+    int numActas[n]={lima, 6000, 12000, 8000, 12000, 15000, extran, 4500};
+    int dp[n+1][totalA+1]={};
+
+    //llenamos el dp
+    for (int i=0;i<n+1; i++) dp[i][0]=1;
+    for (int j=1;j<totalA+1; j++) dp[0][j]=0;
+
+    for (int i=1; i<n+1; i++) {
+        for (int j=1; j<totalA+1; j++) {
+            if (j<numActas[i]) {
+                dp[i][j]=dp[i-1][j];
+            }else {
+                dp[i][j]=dp[i-1][j-numActas[i-1]];
+            }
+        }
+    }
+
+}
---
+++ PreparaciónLab02/LAB02-2026-1/P2/main.h
@@ -0,0 +1,8 @@
+//
+// Created by User on 18/09/2026.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#endif //MAIN_H
